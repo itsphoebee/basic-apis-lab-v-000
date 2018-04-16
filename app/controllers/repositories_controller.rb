1baa2ac9e7ff6aa7b79d914cc2956fc8370573f2
Phoebe Lau <phoebelau7@gmail.com>
2018-04-16 02:06:11 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,7 +6,7 @@ class RepositoriesController < ApplicationController
         req.params['client_id'] = '8cb54fc357265938ea13'
         req.params['client_secret'] = '76bdecf0fb2bfd27af332b2f49ad6d4563032f35'
         req.params['v'] = '20160201'
-        req.params['query'] = 'coffee shop'
+        req.params['query'] = params[:query]
         req.options.timeout = 100
       end
 
